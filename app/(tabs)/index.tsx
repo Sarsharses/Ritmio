f11358af9b442f6d0ecc66fd25b3d8e2a5eee781
sarsharses <ljotte93@gmail.com>
2025-04-04 22:35:13 +0200

    chore: Move components to right folder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HabitGrid from '@/app/components/ui/HabitGrid';
+import HabitGrid from '@/components/ui/HabitGrid';
 
 export default function HomeScreen() {
     return <HabitGrid/>;
