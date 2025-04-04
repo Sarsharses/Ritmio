21b18edb7510a6417e3b99117e01c9ae881c105d
sarsharses <ljotte93@gmail.com>
2025-04-04 21:31:54 +0200

    feat: Add GridHeader.tsx component

---
+++ app/components/ui/GridHeader.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet} from "react-native";
+
+type Props = {
+    habits: string[];
+};
+
+export default function GridHeader({ habits }: Props) {
+    return (
+        <View style={styles.headerRow}>
+            <Text style={styles.cornerCell}>Day ↓</Text>
+            {habits.map((habit, i) => (
+                <Text key={i} style={styles.headerCell}>{habit}</Text>
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    headerRow: {
+        flexDirection: 'row',
+        marginBottom: 10,
+        alignItems: 'center',
+    },
+    headerCell: {
+        flex: 1,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 14,
+    },
+    cornerCell: {
+        width: 60,
+        fontWeight: 'bold',
+        fontSize: 14,
+    }
+});
